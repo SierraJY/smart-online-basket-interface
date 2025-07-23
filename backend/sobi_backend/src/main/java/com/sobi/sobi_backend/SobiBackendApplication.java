b8d7fd4dee7b1f2dad4eff01afdda9feda7872b8
박주연 <wndus51445@gmail.com>
2025-07-23 13:12:11 +0900

    build: Initialize SpringBoot
    S13P11B103-59
    
    Dependencies:
    -Spring Web
    -Spring Data JPA
    -PostgresSQL Driver
    
    Note:
    - Change Branch Name to backend-spring_init

---
+++ backend/sobi_backend/src/main/java/com/sobi/sobi_backend/SobiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sobi.sobi_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SobiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SobiBackendApplication.class, args);
+	}
+
+}
